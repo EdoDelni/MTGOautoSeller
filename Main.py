e2d4aca178c921589437de6d897b05467b46adf4
edo <edoardodrd@gmail.com>
2024-06-03 11:35:45 -0600

    50%completed

--- Main.py
+++ Main.py
@@ -3,8 +3,15 @@ import time
 import pyautogui
 import pytesseract
 from PIL import ImageGrab, Image
-
+import fuzz
+from mss import mss
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from pytesseract import Output
+from mss import mss
+import requests
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files (x86)/Tesseract OCR/tesseract.exe"
+
 def startmtgoapp():
     os.startfile("C:/Users/edo/Desktop/Magic The Gathering Online .appref-ms")
     time.sleep(3)
@@ -15,13 +22,47 @@ def startmtgoapp():
     pyautogui.press("enter")
     time.sleep(15)
 
+def clickonscreen(TargetText, confidence_threshold=50, fuzz_threshold=90):
+    screenshot_path = "screenshot.png"
+    with mss() as sct:
+        sct.shot(output=screenshot_path)
 
-def clickonscreen(TargetText, confidence_threshold=50):
+    image = Image.open(screenshot_path)
+    data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+
+    print(data)
+    found = False
+    TargetText = TargetText.strip()
+    target_words = TargetText.split()
+    target_length = len(target_words)
+
+    for i in range(len(data['text']) - target_length + 1):
+        words = data['text'][i:i + target_length]
+        confidences = data['conf'][i:i + target_length]
+        ocr_phrase = ' '.join([word.strip() for word in words])
+
+        if all(int(confidences[j]) >= confidence_threshold for j in range(target_length)):
+            match_ratio = fuzz.ratio(ocr_phrase, TargetText)
+            if match_ratio >= fuzz_threshold:
+                x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+                center_x, center_y = x + w // 2, y + h // 2
+                found = True
+                break
+
+    if not found:
+        raise ValueError(f"'{TargetText}' not found with sufficient confidence in the screenshot.")
+
+    pyautogui.moveTo(center_x, center_y)
+    pyautogui.click()
+    os.remove(screenshot_path)
+
+def clickonscreenold(TargetText, confidence_threshold=50):
     screenshot_path = "screenshot.png"
     screenshot = ImageGrab.grab()
     screenshot.save(screenshot_path)
     image = Image.open(screenshot_path)
     data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+    print(data)
     found = False
     TargetText = TargetText.strip()
     target_words = TargetText.split()
@@ -42,7 +83,6 @@ def clickonscreen(TargetText, confidence_threshold=50):
 
     if not found:
         raise ValueError(f"'{TargetText}' not found with sufficient confidence in the screenshot.")
-
     pyautogui.moveTo(center_x, center_y)
     pyautogui.click()
     os.remove(screenshot_path)
@@ -73,6 +113,7 @@ def clickonscreenrestricted(TargetText, region=(0, 0, 1920, 1080), confidence_th
 
     if not found:
         raise ValueError(f"'{TargetText}' not found with sufficient confidence in the screenshot.")
+        print(data)
 
     pyautogui.moveTo(center_x, center_y)
     pyautogui.click()
@@ -87,23 +128,36 @@ def clickonimage(relative_path):
     element = pyautogui.locateOnScreen(filename,confidence=0.7)
     pyautogui.click(element)
 
-CardName = "Force Of Will"
+
+def AddCardsToOpenBinder(CardName):
+    clickonscreen("COLLECTION")
+    time.sleep(10)
+    clickonimage("Images\Add Binder.png")
+    time.sleep(2)
+    pyautogui.press("enter")
+    i = 19
+    while i<=0:
+        pyautogui.press("Tab")
+        i = i-1
+    pyautogui.typewrite("BOT BINDER")
+    time.sleep(2)
+    clickonscreen("Search for text on cards")
+    pyautogui.typewrite(CardName)
+    pyautogui.press("enter")
+    time.sleep(3)
+    pyautogui.moveTo(400, 500)
+    pyautogui.rightClick()
+    time.sleep(2)
+    clickonscreen("Select All")
+    time.sleep(3)
+    pyautogui.moveTo(400, 150)
+    pyautogui.rightClick()
+    time.sleep(1)
+    clickonscreenold("Add All to Open Binder")
+
+#main software
 
 startmtgoapp()
-clickonscreen("COLLECTION")
-time.sleep(10)
-clickonimage("Images\Add Binder.png")
-time.sleep(2)
-pyautogui.press("enter")
-time.sleep(2)
-clickonscreen("Search for text on cards")
-pyautogui.typewrite(CardName)
-pyautogui.press("enter")
-time.sleep(3)
-clickonscreenrestricted(CardName, (300, 0, 1980, 1080))
-pyautogui.rightClick()
-time.sleep(1)
-clickonscreen("Add All to Open Binder")
-
-
+CardName = "Lava Dart"
+AddCardsToOpenBinder("Lava dart")
 
